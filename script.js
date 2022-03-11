cbb732ebf962cdb71c168e8135ce145a36542ad1
student <89061703+arannyakarak@users.noreply.github.com>
2022-03-10 20:44:51 -0800

    add EventListener for big image

--- script.js
+++ script.js
@@ -99,3 +99,63 @@ function clicked(e) {
     }
   });
 }
+
+
+//Big image
+const galleryItem = document.querySelectorAll(".all");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+const next = document.querySelector(".next2");
+const prev = document.querySelector(".previous");
+
+var idx_src_arr = {};
+var current_idx = 0;
+
+//clicking on image item
+galleryItem.forEach((item, i) => {
+  idx_src_arr[i] = item.children[0].src;
+  item.addEventListener("click", function(){
+    current_idx = i;
+    // console.log(item.children[0].src);
+    overlay.classList.add("show");
+    modal.classList.add("show");
+    modal.children[0].src=item.children[0].src;
+  });
+})
+
+//close button
+close.addEventListener("click", function(){
+    overlay.classList.remove("show");
+    modal.classList.remove("show");
+})
+
+let show_modal = (src_str) => {
+  modal.children[0].src = src_str;
+}
+
+//next
+next.addEventListener('click', function(){
+  // console.log("next");
+  //showSlide(slideIdx++);
+  if(current_idx == galleryItem.length - 1){
+    current_idx = 0;
+  }
+  else{
+    current_idx = current_idx + 1;
+  }
+  show_modal(idx_src_arr[current_idx]);
+})
+//back
+prev.addEventListener('click', function(){
+  if(current_idx == 0){
+    current_idx = galleryItem.length - 1;
+  }
+  else{
+    current_idx = current_idx - 1;
+  }
+  show_modal(idx_src_arr[current_idx]);
+})
+
+
+
